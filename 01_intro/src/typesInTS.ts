8216b46ef895df58d7129324c929547a28164879
prathmesh-gh <majagaonkar0@gmail.com>
2026-02-21 19:03:59 +0530

    data annotations

---
+++ 01_intro/src/typesInTS.ts
@@ -0,0 +1,22 @@
+//Type annotations and interface in Typescript
+
+let drink ="chai"
+
+//common errror 
+//Type 'number' is not assignable to type 'string'.ts(2322) 
+//because  we assign the number value to string it requires a string 
+
+// let  channelName:string
+// channelName=1234
+
+
+//annotation means colon + string
+
+let chaiFlavour:string= " masala chai"
+chaiFlavour="Ginger chai"
+
+//type of annotations : batana padta hai data type
+
+// let chsiOrder:string
+// let chsiOrder:number
+// let chsiOrder:boolean
